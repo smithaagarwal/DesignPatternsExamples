c2c44e48ec0e6c007e6a4beb78acfe55fb8db8ca
Smitha <smitha.kotian@gmail.com>
2024-07-09 09:56:22 +0100

    feat:demonstrate eager evaluation

--- src/main/java/com/techreturners/Main.java
+++ src/main/java/com/techreturners/Main.java
@@ -3,8 +3,9 @@ package com.techreturners;
 public class Main {
     public static void main(String[] args) {
     int value = 4 ;
-    if (value>4 && LazyEvaluation.compute(value)>100) {
-        System.out.println("Path 1");
+    int temp = LazyEvaluation.compute(value); //eager evaluation, function is evaluated when its called. It does't wait to see if the value from the function is used eventually or not
+    if (value>4 && temp >100) {
+        System.out.println("Path 1 with " + temp);
     } else {
         System.out.println("Path 2");
     }
