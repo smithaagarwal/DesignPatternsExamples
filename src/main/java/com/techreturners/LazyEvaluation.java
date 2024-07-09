60e0fb660f1fbe6ea0d502348bbd5695ac349ef4
Smitha <smitha.kotian@gmail.com>
2024-07-09 09:51:04 +0100

    feat:demonstrate short circuiting

---
+++ src/main/java/com/techreturners/LazyEvaluation.java
@@ -0,0 +1,8 @@
+package com.techreturners;
+
+public class LazyEvaluation {
+    public static int compute(int number) {
+        System.out.println("Compute");
+        return number*100;
+    }
+}
--- src/main/java/com/techreturners/Main.java
+++ src/main/java/com/techreturners/Main.java
@@ -2,6 +2,12 @@ package com.techreturners;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    int value = 4 ;
+    if (value>4 && LazyEvaluation.compute(value)>100) {
+        System.out.println("Path 1");
+    } else {
+        System.out.println("Path 2");
+    }
+
     }
 }
