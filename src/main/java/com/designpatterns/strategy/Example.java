ae4acb4bacfb932a44d10089e7b41886118d45b4
Smitha <smitha.kotian@gmail.com>
2024-07-15 13:48:54 +0100

    refactor: using strategy pattern

--- src/main/java/com/designpatterns/strategy/Example.java
+++ src/main/java/com/designpatterns/strategy/Example.java
@@ -1,18 +1,31 @@
 package com.designpatterns.strategy;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Example {
-    public static int totalValues(List<Integer> numbers) {
+    public static int totalValues(List<Integer> numbers, Predicate<Integer> selector) {
         int total = 0;
-        for(int i:numbers) {
-            total += i;
+        for (int number : numbers) {
+            if (selector.test(number))
+                total += number;
         }
         return total;
     }
 
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+
     public static void main(String[] args) {
-        var numbers = List.of(1,2,3,4,5,6,7,8,9,10);
-        System.out.println(totalValues(numbers));
+        var numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println(totalValues(numbers, ignore -> true));
+        System.out.println(totalValues(numbers, number -> number % 2 == 0));
+        System.out.println(totalValues(numbers, Example::isOdd));
     }
 }
+
+/*
+Strategies are often a single method or function
+So functional interfaces and lambdas work really well
+ */
