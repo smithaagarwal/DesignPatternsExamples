4d6f5438fb8b07b454a005d4ec146aa44e87d952
Smitha <smitha.kotian@gmail.com>
2024-07-15 13:31:34 +0100

    feat: Initial code

---
+++ src/main/java/com/designpatterns/strategy/Example.java
@@ -0,0 +1,17 @@
+package com.designpatterns.strategy;
+
+import java.util.List;
+
+public class Example {
+    public static int totalValues(List<Integer> numbers) {
+        int total = 0;
+        for(int i:numbers) {
+            total += i;
+        }
+        return total;
+    }
+    public static void Main(String[] args) {
+        var numbers = List.of(1,2,3,4,5,6,7,8,9,10);
+        System.out.println(totalValues(numbers));
+    }
+}
