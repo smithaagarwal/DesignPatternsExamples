d4d0ba66a6fae9efd8af84f4ee54e09dd5c05ab3
Smitha <smitha.kotian@gmail.com>
2024-07-15 13:36:26 +0100

    feat: Odd total code

--- src/main/java/com/designpatterns/strategy/Example.java
+++ src/main/java/com/designpatterns/strategy/Example.java
@@ -20,9 +20,19 @@ public class Example {
         return total;
     }
 
-    public static void Main(String[] args) {
+    public static int totalOddValues(List<Integer> numbers) {
+        int total = 0;
+        for(int i:numbers) {
+            if(i%2==0)
+                total += i;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
         var numbers = List.of(1,2,3,4,5,6,7,8,9,10);
         System.out.println(totalValues(numbers));
         System.out.println(totalEvenValues(numbers));
+        System.out.println(totalOddValues(numbers));
     }
 }
