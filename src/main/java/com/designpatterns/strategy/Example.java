cd8ba9c5cf54a7119c368d012435655ee97a71a9
Smitha <smitha.kotian@gmail.com>
2024-07-15 13:33:25 +0100

    feat: Even total code

--- src/main/java/com/designpatterns/strategy/Example.java
+++ src/main/java/com/designpatterns/strategy/Example.java
@@ -10,8 +10,19 @@ public class Example {
         }
         return total;
     }
+
+    public static int totalEvenValues(List<Integer> numbers) {
+        int total = 0;
+        for(int i:numbers) {
+            if(i%2==0)
+                total += i;
+        }
+        return total;
+    }
+
     public static void Main(String[] args) {
         var numbers = List.of(1,2,3,4,5,6,7,8,9,10);
         System.out.println(totalValues(numbers));
+        System.out.println(totalEvenValues(numbers));
     }
 }
